6fe3d8cdbdb9b01ba2ad1083a65be525938d2af7
meisibley <mkong99@yahoo.com>
2023-09-06 11:12:20 -0700

    2-args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if len(argv) == 2:
     print("1 argument:")
     print(f"1: {argv[1]}")
 elif len(argv) == 1:
-    print("0 arguments")
+    print("0 arguments.")
 else:
     print(f"{len(argv) - 1} arguments:")
     for i in range(1, len(argv)):
