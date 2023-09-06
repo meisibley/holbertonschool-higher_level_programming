52d492e51be8d6bd32e789e1c25dd2eb48713567
meisibley <mkong99@yahoo.com>
2023-09-06 09:06:38 -0700

    0-add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-add = __import__('add_0').add
+from add_0 import add
 a, b = 1, 2
 c = add(a, b)
-print(f"{a} + {b} = {c}")
+if __name__ == "__main__":
+    print("{0} + {1} = {2}".format(a, b, c))
