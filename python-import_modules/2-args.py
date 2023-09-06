eee1fa269619a01d8cb41549c21cd22226eb26da
meisibley <mkong99@yahoo.com>
2023-09-06 11:09:07 -0700

    2-args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+if len(argv) == 2:
+    print("1 argument:")
+    print(f"1: {argv[1]}")
+elif len(argv) == 1:
+    print("0 arguments")
+else:
+    print(f"{len(argv) - 1} arguments:")
+    for i in range(1, len(argv)):
+        print(f"{i}: {argv[i]}")
