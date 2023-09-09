63d5cf301cf494ae8a115ee06fbc66fab1a062f2
meisibley <mkong99@yahoo.com>
2023-09-09 06:19:45 -0700

    1-search_replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    if my_list:
         new_list = []
         for i in my_list:
             if i == search:
@@ -7,3 +8,5 @@ def search_replace(my_list, search, replace):
             else:
                 new_list.append(i)
         return new_list
+    else:
+        return my_list
