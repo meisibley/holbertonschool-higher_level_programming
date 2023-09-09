f4dc3b8df2b93eb7b6074deacff0822cdf73b08c
meisibley <mkong99@yahoo.com>
2023-09-09 09:41:21 -0700

    9-multiply_by_2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     if a_dictionary:
-        for x in a_dictionary:
-            a_dictionary[x] = a_dictionary[x] * 2
-    return a_dictionary
+        b_dictionary = {}
+        for x, y in a_dictionary.items():
+            b_dictionary.update({x: y * 2})
+    return b_dictionary
