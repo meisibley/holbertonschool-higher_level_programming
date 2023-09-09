9da2749ae93148129575fc2ccb43bb3c8f87bffc
meisibley <mkong99@yahoo.com>
2023-09-09 07:32:56 -0700

    2-uniq_add.py

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list:
-        result = 0
-        uniq = 0
-        for i in range(len(my_list)):
-            result += my_list[i]
-            for j in range(i + 1, len(my_list)):
-                if my_list[i] == my_list[j]:
-                    uniq += my_list[j]
-        result -= uniq
-        return result
-    else:
-        return None
+    result = sum(set(my_list))
+    return result
