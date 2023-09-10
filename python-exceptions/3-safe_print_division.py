79b30499c76e10df89c085b4c4fc674c887e740d
meisibley <mkong99@yahoo.com>
2023-09-09 21:24:13 -0700

    3-safe_print_division

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,7 +2,8 @@
 def safe_print_division(a, b):
     result = 0
     try:
-        result = a / b
+        if isinstance(a, int) and isinstance(b, int)
+            result = a / b
     except ZeroDivisionError:
         result =  None
     finally:
