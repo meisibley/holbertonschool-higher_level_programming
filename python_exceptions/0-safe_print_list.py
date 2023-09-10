1e7b4c8376fd847c913967095cc8eaef5bc6acaa
meisibley <mkong99@yahoo.com>
2023-09-09 17:39:56 -0700

    0-safe_print_list

--- python_exceptions/0-safe_print_list.py
+++ python_exceptions/0-safe_print_list.py
@@ -6,6 +6,8 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             count = count + 1
         except IndexError:
-            continue
-    print("")
-    return count
+            print()
+            return count
+        else:
+            print()
+            return count
