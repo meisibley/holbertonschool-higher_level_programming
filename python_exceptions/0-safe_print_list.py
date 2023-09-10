da9eacc61a11baf858e87b55db4629748789a1ec
meisibley <mkong99@yahoo.com>
2023-09-09 17:16:22 -0700

    0-safe_print_list

--- python_exceptions/0-safe_print_list.py
+++ python_exceptions/0-safe_print_list.py
@@ -5,9 +5,9 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end="")
             count += 1
-        print("")
+        print()
         return count
     except:
-        print("")
+        print()
         return count
 
