d68faf2d64ed1df9380fe4df15c0c17864537e3c
meisibley <mkong99@yahoo.com>
2023-09-14 14:58:15 -0700

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a squre"""
+
+
+class Square:
+    """private instance attribute: size"""
+
+    def __int__(self, size):
+        self.__size = size
