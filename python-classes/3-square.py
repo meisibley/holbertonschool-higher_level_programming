b053cbf276f8eba6db4649f34485f401267b68c9
meisibley <mkong99@yahoo.com>
2023-09-14 17:25:27 -0700

    3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a squre"""
+
+
+class Square:
+    """private instance attribute: __size"""
+
+    def __init__(self, size=0):
+        self.__size = size
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
