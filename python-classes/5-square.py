feb0f29e69c1ca4e4409b414e8d383b1ad669cae
meisibley <mkong99@yahoo.com>
2023-09-14 19:24:21 -0700

    5-square.py

---
+++ python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a squre"""
+
+
+class Square:
+    """private instance attribute: __size"""
+
+    @property
+    def size(self):
+        """retrieve size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def __init__(self, size=0):
+        """initialize"""
+        self.__size = size
+
+    def area(self):
+        """gets area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("{}".format("#"), end="")
+                print()
