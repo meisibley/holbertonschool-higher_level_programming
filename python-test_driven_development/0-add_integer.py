c0ab69290bcfc1eb8f1fa8ce7827e82e2abcbf00
meisibley <mkong99@yahoo.com>
2023-09-15 18:34:44 -0700

    0-add_integer

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+adds 2 integers;
+2 input numbers a and/or b could be float types, but must be first 
+casted to integers;
+b has initialed value, 98.
+"""
+
+def add_integer(a, b=98):
+    """a function adds 2 integers"""
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    elif type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
---
+++ python-test_driven_development/README.md
@@ -0,0 +1,10 @@
+# Python - Test-driven development
+
+## 0 - add integer
+Write a function that adds 2 integers, and test it
+
+## 2 - matrix divided
+Write a function that divides all elements of a matrix, and test it
+
+## 3 - say my name
+Write a function that prints My name is <first name> <last name>
