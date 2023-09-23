c554de13868db84b1d702d36f847904399d35c4e
meisibley <mkong99@yahoo.com>
2023-09-22 21:31:34 -0700

    8-rectangle.py

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module of Rectangle, a child class of BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """class Rectangle inherited from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """check width and height are positive integers"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
