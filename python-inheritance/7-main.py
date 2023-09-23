c595b1c70756c3f2e31e1bbb29a5bc023194d1cf
meisibley <mkong99@yahoo.com>
2023-09-22 18:12:14 -0700

    7-base_geometry.py

--- python-inheritance/7-main.py
+++ python-inheritance/7-main.py
@@ -9,14 +9,61 @@ bg.integer_validator("width", 89)
 try:
     bg.integer_validator("name", "John")
 except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    print("{} {}".format(e.__class__.__name__, e))
 
 try:
     bg.integer_validator("age", 0)
 except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    print("{} {}".format(e.__class__.__name__, e))
 
 try:
     bg.integer_validator("distance", -4)
 except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    print("{} {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("tooMany inputs", 2, 4)
+except Exception as e:
+    print("{} {}".format(e.__class__.__name__, e))
+
+"""
+try:
+    bg.integer_validator(, 32)
+except Exception as e:
+    print("{} {}".format(e.__class__.__name__, e))
+"""
+
+try:
+    bg.integer_validator("number")
+except Exception as e:
+    print("{} {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator()
+except Exception as e:
+    print("{} {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("name", [1, 2])
+except Exception as e:
+    print("{} {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("name", (1, 2))
+except Exception as e:
+    print("{} {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("name", {0: 1, 1: 3})
+except Exception as e:
+    print("{} {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("name", True)
+except Exception as e:
+    print("{} {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("name", None)
+except Exception as e:
+    print("{} {}".format(e.__class__.__name__, e))
