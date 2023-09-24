141c4c4ad5802ebf705d35443bc000e7feddf796
meisibley <mkong99@yahoo.com>
2023-09-24 09:29:45 -0700

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,13 +4,15 @@
 
 import json
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+save_to_json = __import__('5-save_to_json_file').save_to_json_file
+load_fr_json = __import__('6-load_from_json_file').load_from_json_file
 p_list = []
 for i in range(1, len(sys.argv)):
     p_list.append(str(sys.argv[i]))
 try:
-    with open("add_item.json", mode="x", encoding="utf-8") as a_json:
-        save_to_json_file(p_list, "add_item.json")
+    with open("add_item.json", mode="x") as a_json:
+        save_to_json(p_list, "add_item.json")
 except:
-    save_to_json_file(load_from_json_file("add_item.json") + p_list, "add_item.json")
+    save_to_json(load_fr_json("add_item.json") + p_list, "add_item.json")
