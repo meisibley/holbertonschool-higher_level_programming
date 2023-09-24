41b75f79cdd356b06f331242a6fb3b63b8e68229
meisibley <mkong99@yahoo.com>
2023-09-24 12:12:39 -0700

    11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""a class Student"""
+
+
+class Student:
+    """a class of Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieve dict representation"""
+        cp_dict = self.__dict__.copy()
+        l_item = {}
+        if type(attrs) == list:
+            for i in cp_dict:
+                for j in range(len(attrs)):
+                    if i == attrs[j]:
+                        l_item[i] = cp_dict[i]
+            return l_item
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all attrs of the Student instance"""
+        for i in json:
+            setattr(self, i, json[i])
+
