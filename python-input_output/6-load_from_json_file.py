8510324d105976802b591457c30f122d5b951cbb
meisibley <mkong99@yahoo.com>
2023-09-23 22:40:31 -0700

    6-load_from_json_file.py

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a function that creates an Object from a JSON file"""
+
+
+def load_from_json_file(filename):
+    """convert from JSON to Python file"""
+
+    import json
+    with open(filename, encoding="utf-8") as p_file:
+        return json.load(p_file)
