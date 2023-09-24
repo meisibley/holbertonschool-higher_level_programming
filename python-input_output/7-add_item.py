38e118e23d12aac40e39a5b59b639eb11e3a3d7c
meisibley <mkong99@yahoo.com>
2023-09-24 09:26:22 -0700

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,8 +2,8 @@
 """add all arguments to a Python list and then save to a JSON file"""
 
 
-import sys
 import json
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 p_list = []
