a68e7345c7557efbb230228a67ceb7102f637434
meisibley <mkong99@yahoo.com>
2023-09-24 09:22:44 -0700

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,11 +8,8 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 p_list = []
-i = 0
-for p_arg in sys.argv:
-    if i > 0:
-        p_list.append(str(p_arg))
-    i += 1
+for i in range(1, len(sys.argv)):
+    p_list.append(str(sys.argv[i]))
 try:
     with open("add_item.json", mode="x", encoding="utf-8") as a_json:
         save_to_json_file(p_list, "add_item.json")
