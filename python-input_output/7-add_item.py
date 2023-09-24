a00404784cc92394918d014127050df2df046ffe
meisibley <mkong99@yahoo.com>
2023-09-24 09:32:48 -0700

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,13 +4,14 @@
 
 import json
 import sys
-
-
 save_to_json = __import__('5-save_to_json_file').save_to_json_file
 load_fr_json = __import__('6-load_from_json_file').load_from_json_file
 p_list = []
-for i in range(1, len(sys.argv)):
-    p_list.append(str(sys.argv[i]))
+i = 0
+for p_arg in sys.argv:
+    if i > 0:
+        p_list.append(p_arg)
+    i += 1
 try:
     with open("add_item.json", mode="x") as a_json:
         save_to_json(p_list, "add_item.json")
