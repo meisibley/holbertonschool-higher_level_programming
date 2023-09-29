d29db8d3333f2b8a9a4b74dae759ae1bd4e1580b
meisibley <mkong99@yahoo.com>
2023-09-28 20:48:31 -0700

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,22 +9,23 @@ class Rectangle(Base):
     """child class Rectangle"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Rectangle class constructor"""
         self.width = width
         self.height = height
         self.x = x
         self.y = y
         super().__init__(id)
 
-    def integer_validator(self, attribut, value):
+    def integer_validator(self, attri, value):
         """attribute integer validate"""
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(attribut))
-        if attribut == "width" and value <= 0:
-            raise ValueError("{} must be > 0".format(attribut))
-        if attribut == "height" and value <= 0:
-            raise ValueError("{} must be > 0".format(attribut))
-        if (attribut == "x" and value < 0) or (attribut == "y" and value < 0):
-            raise ValueError("{} must be >= 0".format(attribut))
+            raise TypeError("{} must be an integer".format(attri))
+        if attri == "width" and value <= 0:
+            raise ValueError("{} must be > 0".format(attri))
+        if attri == "height" and value <= 0:
+            raise ValueError("{} must be > 0".format(attri))
+        if (attri == "x" and value < 0) or (attri == "y" and value < 0):
+            raise ValueError("{} must be >= 0".format(attri))
 
     @property
     def width(self):
@@ -39,11 +40,13 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """width setter"""
+        self.integer_validator("width", value)
         self.__width = value
 
     @height.setter
     def height(self, value):
         """height setter"""
+        self.integer_validator("height", value)
         self.___height = value
 
     @property
@@ -59,8 +62,11 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """x setter"""
+        self.integer_validator("x", value)
         self.__x = value
 
     @y.setter
     def y(self, value):
+        """y setter"""
+        self.integer_validator("y", value)
         self.__y = value
