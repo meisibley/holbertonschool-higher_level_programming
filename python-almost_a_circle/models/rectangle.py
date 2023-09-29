1556fbb2aa8ce4bf2aec6ac1054e20b66f4f4449
meisibley <mkong99@yahoo.com>
2023-09-29 06:24:30 -0700

    rect

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -10,11 +10,11 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Rectangle class constructor"""
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
     def integer_validator(self, attri, value):
         """attribute integer validate"""
