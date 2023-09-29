ac903aefadebb188477da87ac252b48eebb29586
meisibley <mkong99@yahoo.com>
2023-09-29 09:37:17 -0700

    7-display1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -78,7 +78,11 @@ class Rectangle(Base):
 
     def display(self):
         """display the rectangle with #"""
+        for n in range(self.__y):
+            print()
         for i in range(self.__height):
+            for m in range(self.__x):
+                print(" ", end="")
             for j in range(self.__width):
                 print("{}".format("#"), end="")
             print()
