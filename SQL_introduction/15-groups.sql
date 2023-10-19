e64aa69e914e0e2afe07b8c11f83086c1762d5eb
meisibley <mkong99@yahoo.com>
2023-10-19 13:48:08 -0700

    15-groups.sql

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists the number of records w/ same score in second_table of hbtn_0c_0
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score DESC;
