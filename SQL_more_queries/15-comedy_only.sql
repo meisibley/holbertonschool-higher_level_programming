85ce0b61bbf6a748fa11d020393b6a91bdbce0ff
meisibley <mkong99@yahoo.com>
2023-10-21 14:36:49 -0700

    15-comedy_only.sql

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -4,5 +4,5 @@
 -- Results must be sorted in ascending order by the show title
 SELECT title FROM tv_shows INNER JOIN
 (tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id)
-ON tv_shows.id = tv_show_genres.genre_id
+ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_genres.name = 'Comedy' ORDER BY tv_shows.title ASC;
