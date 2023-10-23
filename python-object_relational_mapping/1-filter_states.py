73bbe2dd9c447ba7c3d3d619270080468a5a093c
meisibley <mkong99@yahoo.com>
2023-10-23 11:26:29 -0700

    1-filter_states.py

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Write a script that lists all states starting with N from hbtn_0e_0_usa"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cursor.fetchall()
+    for i in range(len(states)):
+        if states[i][1][0] == "N":
+            print(states[i])
+    cursor.close()
+    db.close()
