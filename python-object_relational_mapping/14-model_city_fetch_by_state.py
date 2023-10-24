01cfccb1195ddb9ba59147c6f07d67a5a8bbcc2d
meisibley <mkong99@yahoo.com>
2023-10-24 13:49:32 -0700

    14-model_city_fetch_by_state

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""prints all City objects from the database hbtn_0e_14_usa"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_city import City
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    new_session = sessionmaker(bind=engine)
+    session = new_session()
+    city_session = session.query(City, State).\
+            filter(City.state_id == State.id).all()
+    for city, state in city_session:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+python file contains class definit of City and Base = declarative_base():
+City class: inherits from Base Tips; links to the MySQL table states
+class attr id represents a column of an auto-generated, unique integer,
+can’t be null and is a primary key
+class attr name represents a column of string w/ max 128 chars and not null
+class attr state_id represents a column of an integer, can’t be null and 
+is a foreign key to states.id
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.schema import ForeignKey
+from model_state import Base, State
+
+
+Base = declarative_base()
+
+
+class City(Base):
+    """inherit from Base, City is an ORM class, links to table cities"""
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
