d728cfb275415144d21673b4896f5d28595e82d9
meisibley <mkong99@yahoo.com>
2023-11-23 16:26:59 -0800

    1-multi_languages.js

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const f_line = 'C is fun';
+const s_line = 'Python is cool';
+const th_line = 'JavaScript is amazing';
+console.log(f_line);
+console.log(s_line);
+console.log(th_line);
