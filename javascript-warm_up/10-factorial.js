717d462b0ded4d9c28b940479b7b5ac5dfc83881
meisibley <mkong99@yahoo.com>
2023-11-24 19:59:37 -0800

    10-factorial.js

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const arg = parseInt(process.argv[2]);
+function factorial(ar) {
+  if (isNaN(ar) === true || ar === 1) {
+    return (1);
+  } else {
+    return ar * factorial(ar - 1);
+  }
+}
+console.log(factorial(arg));
