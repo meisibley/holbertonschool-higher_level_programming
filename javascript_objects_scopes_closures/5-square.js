3597d47381da7fd36a3f04a86f1a15b850a723ee
meisibley <mkong99@yahoo.com>
2023-11-25 15:33:43 -0800

    5-square.js

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super();
+    size = parseInt(size);
+    if (size > 0) {
+      this.width = size;
+      this.height = size;
+    }
+  }
+}
+module.exports = Square;
