beb85aedb727424192b2ea8e8706693d04d95aa3
meisibley <mkong99@yahoo.com>
2023-11-25 14:14:11 -0800

    3-rectangle.js

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    w = parseInt(w);
+    h = parseInt(h);
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+Rectangle.prototype.print = function () {
+  for (let row = 1; row <= this.height; row++) {
+    const printX = 'X'.repeat(this.width);
+    console.log(printX);
+  }
+}
+
+module.exports = Rectangle;
