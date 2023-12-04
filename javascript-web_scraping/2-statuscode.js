5520fb86fb24eb040e0d8e69757c409fdf2ab039
meisibley <mkong99@yahoo.com>
2023-12-04 23:31:12 +0000

    2-statuscode.js

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 
-request(process.argv[2], function(err, response, body) {
+request(process.argv[2], function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
